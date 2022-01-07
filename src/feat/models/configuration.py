ba656fd89e33f1d36d6a3e24ccba354eba6afc60
AnneliesVlaar <66774020+AnneliesVlaar@users.noreply.github.com>
2022-01-07 14:18:43 +0100

    Initialize toml file with date of creation

--- src/feat/models/configuration.py
+++ src/feat/models/configuration.py
@@ -1,6 +1,42 @@
 import toml
+import datetime
+
+
+class fileIO:
+    def __init__(self):
+        pass
+
+    def init_toml(self):
+        
+        # Check if configuration toml already exist
+        try:
+            open('test.toml', 'r').close()
+        except IOError:
+            # create configuration file
+            open('test.toml', 'w').close()
+
+        # initialize toml file with time and date of creation
+        init_dict = {'title': "Feedback Realisation Exeperimenten Automatisering Konfiguration", 
+                        "data": {"created": datetime.datetime.now()}}
+
+        with open('test.toml', 'w') as f:
+            toml.dump(init_dict,f)
+
 
 class configuration:
     def __init__(self):
-        config = toml.load('config.toml')
-        print(config['project']['project2']['name'])
+
+        fileio = fileIO()
+        
+        # initialise configuration toml
+        fileio.init_toml()
+
+        
+
+
+
+        # config = toml.load('config.toml')
+        # print(config['project']['project2']['name'])
+
+if __name__ == "__main__":
+    configuration()
