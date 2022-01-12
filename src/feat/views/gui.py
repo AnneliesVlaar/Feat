61d174ca891039def2582f2de55101401a822584
AnneliesVlaar <66774020+AnneliesVlaar@users.noreply.github.com>
2022-01-12 17:18:58 +0100

    Open csv file with student names create dictionary from csv file add dictionary to toml add studentnames to combobox

--- src/feat/views/gui.py
+++ src/feat/views/gui.py
@@ -5,7 +5,7 @@ from PyQt5 import QtWidgets, uic
 
 from feat.models.configuration import configuration
 
-import pandas as pd
+
 
 
 
@@ -18,47 +18,32 @@ class UserInterface(QtWidgets.QMainWindow):
         # load feat gui design
         uic.loadUi(pkg_resources.resource_stream("feat.views", 'gui_feat.ui'), self)
 
-        self.config = configuration()
-
-        # # load student names to combobox
-        # TODO: Een open deze file met studenten dingetje
-        # df_students = pd.read_csv("students_A-test.csv", sep=",", header=0, encoding='latin-1')
+        # load students names in toml file
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(self, filter="CSV files (*.csv *.txt)") 
+        self.config = configuration(filename)        
 
         # load data from toml file
         self.config_dict = self.config.open_toml()
+
         
         # add student names to combobox
-        for i,student in enumerate(self.config_dict['students']):
-
-            first = self.config_dict["students"][student]["FirstName"]
-            middle = self.config_dict["students"][student]["MiddleName"]
-            last = self.config_dict["students"][student]["LastName"]
-            
-            # TODO: dit misschien via fileio? veilig wegschrijven van dingen?
-            # add index to toml data 
-            self.config_dict["students"][student]["idx"] = i
-            
-            self.student_comboBox.addItem(first+" "+middle+" "+last)
-
-
+        for student in self.config_dict['students']:
+            self.student_comboBox.addItem(student)
+        
+        # initialise text box
+        self.text_add()
 
         #slots and signals
         self.student_comboBox.currentTextChanged.connect(self.text_add)
 
     
     def text_add(self):
-        
-        # index of current selected student
-        idx = self.student_comboBox.currentIndex()
-
-        # Find out which student is selected
-        current_student = None
-        for student in self.config_dict['students']:
-            if idx == self.config_dict["students"][student]["idx"]:
-                current_student = student
 
         # clear text field
         self.read_only.clear()
+
+        # index of current selected student
+        current_student = self.student_comboBox.currentText()
 
         # add text to text field
         first_line = 'Hoi ' + self.config_dict["students"][current_student]["FirstName"] + ','
