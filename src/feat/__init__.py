9a446860bddd97739876461c89cc737ae02a7406
David Fokkema <davidfokkema@icloud.com>
2024-08-05 12:54:26 +0200

    Bump version to 0.4.0

--- src/feat/__init__.py
+++ src/feat/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '0.1.0'
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("feat-feedback-tool")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = None
